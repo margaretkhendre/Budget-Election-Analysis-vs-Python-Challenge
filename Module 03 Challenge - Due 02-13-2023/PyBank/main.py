26e25e625b23575a58ef2e1f645a5b39ea50551c
Kelly Hendre <121995835+margaretkhendre@users.noreply.github.com>
2023-03-17 13:06:26 -0400

    Python Challenge Revisited
    New attempt

--- Module 03 Challenge - Due 02-13-2023/PyBank/main.py
+++ Module 03 Challenge - Due 02-13-2023/PyBank/main.py
@@ -1,38 +1,98 @@
 # Calculate the total number of months 
 import os
 import csv
-    # create path to csv file
-budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
-print("Financial Analysis")
+# create path to csv file to read data
+budget_data = os.path.join("Resources", "budget_data.csv")
 
-with open(budget_data_csv) as csvFile:
+# text file to hold the output of the revenue analysis
+outputFile = os.path.join("revenueAnalysis.text")
+
+#variables:
+total_Months = 0 # initialize the total to 0
+total_Revenue= 0  # initialize the total to 0
+monthly_Changes = [] # initialize list of monthly net changes
+months= []          # initializes the list of months
+
+# read the csv file
+with open(budget_data) as budget_data:
+    #create a scv reader object
+    csvreader= csv.reader(budget_data)
+
+    #read the header row
+    header = next(csvreader)
+    # move the first row
+    first_Row= next(csvreader)
+
+    #increment the count of the total months
+    total_Months += 1 # same as totalMonths +1 
+
+    # add on to the amount of revenue
+            #revenue is in index 1
+    total_Revenue += float(first_Row[1]) # have to convert variables to readable numbers
     
-    csvReader = csv.reader(csvFile,delimiter= ',')
-    # skip the first row, and establish your count to start at 0
-    rowindex = 0
-    counter = 0 
-    header = next(csvReader)
- 
-    for row in csvReader:  
-        # create a counter to calculate the total number of months and print it 
-        counter = counter + 1
-print(" Total Months:", + counter)
+    #establish the previous revenues
+        #revenue is in index 1
+    previous_Revenue = float(first_Row[1])
 
-# Calculate the net total amount of "Profit/Losses" over the entire period 
-with open(budget_data_csv) as csvFile:
-    csvReader= csv.reader(csvFile, delimiter= ',')
-    
-    total = 0
-    # skip the first row 
-    header = next(csvReader)
+    #break out the rest of the csv file into rows. 
+    #so for every row in our csv reader, we are going to increment 
+    # the count of the total months, and have it skip the first row (because it contains column names)
+    for row in csvreader:
+        #increment the count of the total months
+        total_Months += 1 # same as totalMonths +1 
 
-    for row in csvReader:
-        # calculate the total using the sum function and print it 
-        profitLossesTotal = sum(int(row[1]))
-       
-print(" Total:", profitLossesTotal)
+        # add on to the amount of revenue
+            #revenue is in index 1
+        total_Revenue += float(row[1]) # have to convert variables to readable numbers
         
-# Calculate the changes in "Profit/Losses" over the entire period, and then the average of those changes
-avg = sum(row[1])/ len(row[1])
-print("Total Average:", + avg)
+        # calculate the net change
+        net_Change= float(row[1]) - previous_Revenue
+        # add to the list of monthly changes
+        monthly_Changes.append(net_Change)
+
+        # add the first month that a change occurred
+            # month is in index 0
+        months.append(row[0])
+
+        #update the previous revenue
+        previous_Revenue = float(row[1])
+
+# Calculate the average net change per month 
+average_Change_Per_month = sum(monthly_Changes) / len(monthly_Changes)
+
+greatest_Increase = [months[0], monthly_Changes[0]] # holds the month and the value of the greatest increase
+greatest_Decrease = [months[0], monthly_Changes[0]] # holds the month of and the value of th greatest decrease
+
+#use this loop to calculate the index of the greatest and least monthly change
+for m in range(len(monthly_Changes)):
+    # calculate the greatest increase and decrease
+    if(monthly_Changes[m]> greatest_Increase[1]):
+        # if the value is greater than the greatest increase, that value becomes the new greatest increase
+        greatest_Increase[1]= monthly_Changes[m]
+        # update the month
+        greatest_Increase[0] = months[m]
+
+    if(monthly_Changes[m]< greatest_Decrease[1]):
+        # if the value is less than the greatest decrease, that value becomes the new greatest decrease
+        greatest_Decrease[1]= monthly_Changes[m]
+        # update the month
+        greatest_Decrease[0] = months[m]
+
+# start generating the output for the text file. Call the variable "output". \n = new line, \t = tab
+output= (
+    f"Financial Analysis \n"
+    f"------------------------ \n"
+    f"\t Total Months - {total_Months} \n"
+    f"\t Total Revenue - ${total_Revenue:,.2f}\n" # separate with commas and have 2 decimal points
+    f"\t Average Change per Month - ${average_Change_Per_month:,.2f}\n"
+    f"\t Greatest Increase - {greatest_Increase[0]} Amount $ {greatest_Increase[1]:,.2f}\n"
+    f"\t Greatest Decrease - {greatest_Decrease[0]} Amount $ {greatest_Decrease[1]:,.2f}\n"
+)
+
+# print the output to the terminal 
+print(output)
+
+#export the output variable to the text file
+with open(outputFile, "w") as textfile:
+    textfile.write(output)
--- Module 03 Challenge - Due 02-13-2023/PyPoll/main.py
+++ Module 03 Challenge - Due 02-13-2023/PyPoll/main.py
@@ -1,23 +1,90 @@
+#import the csc and modules
 import os 
 import csv
-# create path to csv file
-election_data_csv = os.path.join("Resources", "election_data.csv")
 
-print("Election Results")
+#load data to read survey data
+input_file = os.path.join("Resources", "election_data.csv")
 
-with open(election_data_csv) as csvFile:
+#output file location for the survey analysis
+output_file= os.path.join("Election Results Analysis.txt")
 
-    csvReader = csv.reader(csvFile, delimiter=',')
-# skip the first row, and establish your count to start at 0
-    rowindex= 0
-    couter = 0
-    header = next(csvReader)
+#variables 
+total_votes= 0 # variables that holds the total number of votes
+candidates= [] # list that holds the candidates in the survey 
+candidate_votes= {} # dictionary that will hold the votes each candidate receives
+winning_count= 0 # holds the winning count
+winning_candidate = "" # variable to hold the winning candidate
 
-    for row in csvReader:
-        # print the first column to make sure you're working with the proper bit of data
-        print(row[0])
-        # calculate the total using sum function and print 
-        total = sum(int(row[0]))
-        print(total)
-print(total)
-print("winner:")
+#read the csv file
+with open(input_file) as surveyData:
+    #create the csv reader
+    csvreader = csv.reader(surveyData)
+
+    #read the header
+    header = next(csvreader)
+
+    #rows will be lists 
+        #index 0 is the ballot id 
+        #index 1 is the county
+        #index 2 is the candidate
+    
+    # for each row
+    for row in csvreader:
+        # add on to the total votes
+        total_votes += 1 #same as total _votes = total_votes +1
+
+        #check to see if the candidate is in the list of candidates
+        if row[2] not in candidates:
+            # if the candidate is not in the list, add the candidate to the list 
+            candidates.append(row[2])
+
+            # add the value to the dictionary as well
+            #("key": value)
+            #start the count at 1 for the votes
+            candidate_votes[row[2]] = 1
+
+        else:
+            # the candidate is in the list of candidates
+            # add a vote to that candidate count
+            candidate_votes[row[2]]+= 1
+
+#print(candidate_votes)
+candidate_output = ""
+for candidates in candidate_votes:
+    #get the vote count and percentage of votes
+    votes= candidate_votes.get(candidates)
+    vote_pct= (float(votes) / float(total_votes)) * 100.00
+    vote_totals= float(votes) 
+
+    #compare votes to winning count
+    if votes > winning_count:
+        # update the votes to be the new winning count
+        winning_count = votes
+        #update the winning candidate
+        winning_candidate = candidates
+
+    #winning candidate output
+    winning_candidate= f"{winning_candidate}\n"
+
+    # percentages with totals output
+    candidate_output += f"\t {candidates}: {vote_pct:.3f}% , ({vote_totals})\n"
+    #print(candidate_output)
+
+#create an output variable to hold the  output
+output= (
+    f"\n\n Election Results\n"
+    f"-----------------------------------------\n"
+    f"\t Total Votes: {total_votes:,} \n" #separate by thousands
+    f"-----------------------------------------\n"
+    f"{candidate_output} \n"
+    f"-----------------------------------------\n"
+    f"\t  Winner: {winning_candidate}"
+
+)
+
+print(output)
+
+#print the results and export the data to the text file 
+with open(output_file, "w") as text_file:
+    #write the output to the fil
+    text_file.write(output)
